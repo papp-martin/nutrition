12a2d66694016d4b2e9999b7cb129147691c6a36
Martin Papp <sze0papmar@vavsari.org>
2022-05-27 17:59:17 +0200

    kivettem

--- src/pages/nutritions-page/nutritions-page-component.jsx
+++ src/pages/nutritions-page/nutritions-page-component.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import ProductsOverview from '../../components/products-overview/products-overview.component';
 import ProductPage from '../product/product.component';
 
 
 const NutritionsPage = ({ match }) => (
     <div className='products-page'>
-        <Route exact path={`${match.path}`} component={ProductsOverview} />
         <Route path={`${match.path}/:productId`} component={ProductPage} />
     </div>
 );
